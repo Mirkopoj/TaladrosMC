ee8e9eeb7e29081ebdee7f439a547225ec79d68c
Mirko <mirkopoj@hotmail.com>
2023-02-06 13:41:28 -0300

    calibracion explotada a su propia funcion

--- calibracion.c
+++ calibracion.c
@@ -1,12 +1,13 @@
 #include "calibracion.h"
+#include "constantes.h"
 #include "mcc_generated_files/mcc.h"
 #include "mcc_generated_files/adc.h"
 #include "mcc_generated_files/pwm1.h"
 #include "auxiliares.h"
 #include "nonvolatile.h"
+#include <stdint.h>
 
-void calibrar(struct botones_t botones){
-   if (botones.boton1 > V_MODO_CALIBRACION && botones.boton2 > V_MODO_CALIBRACION) {
+void guardar_valor_actual(uint16_t *boton1, uint16_t *boton2){
       int16_t C1p = 0;
       int16_t C2p = 0;
 
@@ -14,35 +15,27 @@ void calibrar(struct botones_t botones){
          C1p += ADC_GetConversion(hallC1);
          C2p += ADC_GetConversion(hallC2);
       }
-      BOTON1_MAX = C1p/N_SAMPLES_AVERAGE;
-      BOTON2_MAX = C2p/N_SAMPLES_AVERAGE;
+      *boton1 = C1p/N_SAMPLES_AVERAGE;
+      *boton2 = C2p/N_SAMPLES_AVERAGE;
       
       PWM1_LoadDutyValue(DCmaxBoton1);
       __delay_ms(T_GIRO_MOTOR);
       PWM1_LoadDutyValue(DCmaxBoton2);
       __delay_ms(T_GIRO_MOTOR);
       PWM1_LoadDutyValue(DCmin);
+}
+
+void calibrar(struct botones_t botones){
+   if (botones.boton1 > V_MODO_CALIBRACION && botones.boton2 > V_MODO_CALIBRACION) {
+
+		guardar_valor_actual(&BOTON1_MAX, &BOTON2_MAX);
       
-      C1p = 0;
-      C2p = 0;
-       
       while (botones.boton1 > V_SALIR_CALIBRACION || botones.boton2 > V_SALIR_CALIBRACION) {
 			botones = leer_botones();
       }
       __delay_ms(T_PARA_SOLTAR_BOTONES);
       
-      for (int i=0;i<N_SAMPLES_AVERAGE;i++){
-         C1p += ADC_GetConversion(hallC1);
-         C2p += ADC_GetConversion(hallC2);
-      }
-      BOTON1_MIN = C1p/N_SAMPLES_AVERAGE;
-      BOTON2_MIN = C2p/N_SAMPLES_AVERAGE;
-      
-      PWM1_LoadDutyValue(DCmaxBoton1);
-      __delay_ms(T_GIRO_MOTOR);
-      PWM1_LoadDutyValue(DCmaxBoton2);
-      __delay_ms(T_GIRO_MOTOR);
-      PWM1_LoadDutyValue(DCmin);
+		guardar_valor_actual(&BOTON1_MIN, &BOTON2_MIN);
       
       save_to_nonvolatile();
       
