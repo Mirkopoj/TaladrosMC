ef44066223f2ce2271d036fba7e9aa7055ed1dd2
Mirko <mirkopoj@hotmail.com>
2023-02-13 11:54:38 -0300

    Constantes.c ajustadas

--- constantes.c
+++ constantes.c
@@ -5,16 +5,17 @@ uint16_t BOTON1_MIN = 542; //2,646v
 uint16_t BOTON2_MAX = 656; //3,202v
 uint16_t BOTON2_MIN = 542; //2,646v
 
-const uint16_t DCidle = 374;
+const uint16_t DCidle = 375;
 
 //Taladro = Cw,  Sierra = Rapido
 const uint16_t DCminBoton1 = 
 #ifdef SYSTEM_5
-	398
+	375
 #else
-	342
+	375
 #endif 
 ;
+
 const uint16_t DCmaxBoton1 = 
 #ifdef SYSTEM_5
 	500
@@ -23,25 +24,23 @@ const uint16_t DCmaxBoton1 =
 #endif 
 ;
 
-
 //Taladro = CCw,  Sierra = Rapido
-const uint16_t DCminBoton2 = 398 ;
+const uint16_t DCminBoton2 = 375 ;
 const uint16_t DCmaxBoton2 = 
 #ifdef SYSTEM_5
-	417
+	480
 #else
 	500
 #endif 
 ;
 
-
 //Taladro = 10, Sierra = 3
 const uint16_t DCHumbral = 
 #ifdef SYSTEM_5
-	3
+	2
 #else
-	10
+	2
 #endif 
 ;
 
-const uint16_t DCHisteresis = 10;
+const uint16_t DCHisteresis = 20;
